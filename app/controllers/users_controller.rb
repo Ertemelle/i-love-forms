8d60380c67b51a6f98b1a4a6b9cbf8ebb9828c61
Ertemelle <phenixime@hotmail.fr>
2018-07-31 13:27:43 +0200

    ajout de la page error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,11 @@ class UsersController < ApplicationController
 
   def create
   	@user = User.create(username: params[:username], email: params[:email], bio: params[:bio])
-  	puts "je suis le controller"
+  	if @user.save
+      redirect_to action: 'show', id: @user.username
+      else render '/users/error'
+    end
+   	puts "je suis le controller"
   	puts @user.username
   end
 end
