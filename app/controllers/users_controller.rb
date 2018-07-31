3145f3e8ca1f3abcbe4adae557a2ffae3a4a30b0
Romain <romainsaillour@orange.fr>
2018-07-31 18:10:20 +0200

    essais bootstrap

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,10 @@ class UsersController < ApplicationController
   end
 
   def create
+      #method pour le formulaire form_for
   	@user = User.create(username: params[:user][:username], email: params[:user][:email], bio: params[:user][:bio])
+      #method pour le formulaire form et form_tag
+    #@user = User.create(username: params[:username], email: params[:email], bio: params[:bio])
   	if @user.save
       redirect_to action: 'show', id: @user.username
     else render '/users/error'
